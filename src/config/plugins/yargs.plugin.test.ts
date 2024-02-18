66b75a0c8ca085e8ec3025db9e25aff46fa92a32
Doctoralpaca21 <fflorez_16@hotmail.com>
2024-02-18 11:52:13 -0500

    Test: args with custom values

--- src/config/plugins/yargs.plugin.test.ts
+++ src/config/plugins/yargs.plugin.test.ts
@@ -1,25 +1,44 @@
 // import {yarg} from "./yargs.plugin"
-const runCommand =  async(args: string[]) =>{
+const runCommand = async (args: string[]) => {
     process.argv = [...process.argv, ...args]
 
-    const {yarg} = await import('./yargs.plugin');
+    const { yarg } = await import('./yargs.plugin');
 
     return yarg;
 }
 
-describe('Test args plugins', () => { 
-    test('should return default values', async() => { 
+describe('Test args plugins', () => {
+    const originalArgs = process.argv;
+    beforeEach(()=>{
+        process.argv = originalArgs;
+        jest.resetModules()
+    })
+
+    test('should return default values', async () => {
         const argv = await runCommand(['-b', '9']);
 
         // console.log(argv)
 
         expect(argv).toEqual(expect.objectContaining({
-            b:9,
-            l:10,
-            s:false,
-            n:'table',
+            b: 9,
+            l: 10,
+            s: false,
+            n: 'table',
             d: '/Outputs'
         })
         )
-     })
- })
+    });
+
+    test('should return configuration with custom values', async () => {
+        const argv = await runCommand(['-b', '9', '-l', '20', '-s', '-n','custom-name','-d', 'custom-dir']);
+
+        expect(argv).toEqual(expect.objectContaining({
+            b: 9,
+            l: 20,
+            s: true,
+            n: 'custom-name',
+            d: 'custom-dir'
+        })
+        )
+    })
+})
