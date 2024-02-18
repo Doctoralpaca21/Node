8005497ab2975267122df96bc250dc5f9ac63ef9
Doctoralpaca21 <fflorez_16@hotmail.com>
2024-02-18 09:58:54 -0500

    test: saveFileUseCase with custom values

--- src/domain/use-cases/save-file.use-case.test.ts
+++ src/domain/use-cases/save-file.use-case.test.ts
@@ -27,4 +27,26 @@ describe('SaveFileUseCase', ()=>{
         expect(checkFile).toBe(true);
         expect(fileContent).toBe(options.fileContent)
     });
+
+    test('should save file with custom values', () => { 
+        const options = {
+            fileContent: 'Custom content',
+            destination: 'custom-outputs/file-destination',
+            fileName: 'custom-table-name'
+        }
+        const saveFile = new SaveFile();
+        const filePath = `${options.destination}/${options.fileName}.txt`;
+        const result = saveFile.execute(options);
+
+        expect(result).toBeTruthy();
+
+        const checkFile = fs.existsSync(filePath);
+        const fileContent = fs.readFileSync(filePath,{encoding: 'utf-8'});
+        console.log(fileContent)
+
+
+        expect(checkFile).toBe(true);
+        expect(fileContent).toBe(options.fileContent)
+
+     })
 })
