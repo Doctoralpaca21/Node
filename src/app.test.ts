1f62c62286c8a9e97680c013495f7d795dfa2623
Doctoralpaca21 <fflorez_16@hotmail.com>
2024-02-20 07:35:26 -0500

    Test: AppTest method run with custom values

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,21 @@
-describe('App', () => {
-    test('should be true', () => { 
-        expect(true).toBe(true);
-     })
+import {ServerApp} from './presentation/server-app'
+
+describe('Test App.ts', () => {
+    test('should call Server.run with values', async() => {
+        const serverRunMock = jest.fn();
+        ServerApp.run = serverRunMock;
+        process.argv = ['node','app.ts','-b','10','-l','5','-s','-n','test-file','-d','test-dest'];
+
+        await import('./app');
+
+        expect(serverRunMock).toHaveBeenCalledWith({
+            base: 10,
+            limit: 5,
+            showTable: true,
+            name: 'test-file',
+            destination: 'test-dest'
+        })
+
+
+    })
 })
